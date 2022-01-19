4880302cabe62fab0fa56dbc9e429b4c7e9b8be7
Ashley Fellows <ashley.r.fellows@gmail.com>
2022-01-19 18:55:55 -0500

    added notecreate model

--- ElevenNote.WebMVC/Controllers/NoteController.cs
+++ ElevenNote.WebMVC/Controllers/NoteController.cs
@@ -16,5 +16,11 @@ namespace ElevenNote.WebMVC.Controllers
             var model = new NoteListItem[0];
             return View(model);
         }
+
+        // GET: Note/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
     }
 }
